6174fd8135f09d2d4b932ef936e9a85e1542317c
Rakshitha Kolimi <kolimi.rakshitha@intiqo.com>
2026-01-25 21:20:06 +0530

    mobile verification fix

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -29,7 +29,7 @@ type UserInput struct {
 	FirstName string   `json:"first_name" binding:"required,min=3"`
 	LastName  string   `json:"last_name" binding:"required"`
 	Email     string   `json:"email" binding:"required,email"`
-	Mobile    string   `json:"mobile" binding:"required,mobile"`
+	Mobile    string   `json:"mobile" binding:"required,e164"`
 	Password  string   `json:"password" binding:"required,min=6"`
 	Role      UserRole `json:"role" binding:"required"`
 } // @name UserInput
